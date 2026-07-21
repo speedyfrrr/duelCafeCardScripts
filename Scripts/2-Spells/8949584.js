99b256de2f0245893f6aa7e1cb373ae85a631815
Nick Mirambeau <nickmirambeau@gmail.com>
2026-07-21 01:50:25 -0400

    Update and rename 8949584.json to 8949584.js

---
+++ Scripts/2-Spells/8949584.js
@@ -0,0 +1,27 @@
+const archetype = "Elemental HERO";
+const halfLp = "Pay half your LP";
+
+"commands":[
+  {
+    "trigger_range":[20,24],
+    "msg":"Resolve $name's effect?",
+    "options":[
+      {
+        "label":"$halfLp ONLY",
+        "script":{
+          is_cost;
+          div_lp=2;
+        }
+      },
+      {
+        "label":"$halfLp, then Special Summon 1 Level 4 or lower \"$archetype\" monster from your Deck",
+        "script":{
+          is_cost;
+          div_lp=2;
+          is_eff;
+          search(0,255,[0],"IS_MONSTER && NAME == %$archetype% && LVL <= 4");
+        }
+      }
+    ]
+  }
+]
